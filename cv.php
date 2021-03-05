281013d922778f8f20e27691e236ba006e603fb2
hafauzan <80023557+hafauzan@users.noreply.github.com>
2021-03-05 09:59:03 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,90 @@
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>INFORMASI PRIBADI</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="https://scontent.fcgk3-2.fna.fbcdn.net/v/t1.0-9/117771867_169509201474292_1107278537933616921_o.jpg?_nc_cat=102&ccb=3&_nc_sid=8bfeb9&_nc_eui2=AeEfkLpVR2DQgDQICEZAhkmHGk6aFJNIySIaTpoUk0jJIkun7VcLSSCPrf7yIUdg75ghYWo_y6eoaRjTumbJVODm&_nc_ohc=7klfd7Wiag4AX9bUHVX&_nc_ht=scontent.fcgk3-2.fna&oh=1a22d79edf7345511e7993ec861077da&oe=6067892D" align="right" width="225" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Muhammad Hafidz Fauzan </td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir</td>
+            <td>: Sukabumi, 01 Mei 2004 </td>  
+        </tr>
+            <td>Alamat</td>
+            <td>: Jl. Tata Nugraha No.78, Cicadas Girang 03/03, Jayamekar, Baros, Sukabumi </td>
+        <tr>
+            <td>Status</td>
+            <td>: Pelajar</td>
+        </tr>
+            <tr>
+            <td>NO HP</td>
+            <td>: 087782283433</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: muhafauzan14@gmail</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>PENDIDIKAN FORMAL</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2022</td>
+                <td>SMK Negeri 2 Sukabumi</td>
+            </tr>
+            <tr>
+                <td>2016-2019</td>
+                <td>SMP Negeri 1 Sukabumi </td>  
+            </tr>
+                <td>2010-2016</td>
+                <td>SDN Cicadas Girang</td>
+            <tr>
+                <td>2008-2010</td>
+                <td>TK Al-Inayah</td>
+            </tr>
+            </table>
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANISASI</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2020</td>
+                            <td>Ketua Ekstrakulikuler Bahasa</td>
+                        </tr>
+                        <tr>
+                            <td>2019</td>
+                            <td>Judat Ambalan Kiansantang </td>  
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>KEAHLIAN</b><hr>
+                        Mengoperasikan Komputer : Microsoft Office<br>
+                        Desain Grafis: CorelDRAW <br>
+    <a href="indeks.html">KEMBALI KE INDEKS</a><br>
+    </body>
+</html>
